bfc957705fbd076ce7dad184c92cf3bfc1546bc2
Alex Bieliakov <bieliakov.alex@gmail.com>
2018-05-07 23:53:34 +0300

    Initial commit

---
+++ add_element_before_last.py
@@ -0,0 +1,15 @@
+# демонстрация добавления элемента в список перед последним
+
+test_list = [-1]
+print(test_list)
+test_list.insert(-1, 2)  # для добавления перед последним в списке используется индекс -1
+print(test_list)
+test_list.insert(-1, 3)
+print(test_list)
+test_list.insert(-1, 4)
+print(test_list)
+
+# проверка при пустом списке
+test_list = []
+test_list.insert(-1, 1)  # тогда добавляется в конец
+print(test_list)
